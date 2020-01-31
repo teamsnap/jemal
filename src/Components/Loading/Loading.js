711baee54dba718741e32d2b4a037f801e08dd21
Kyle Bacon <hello@kylebaconcreative.com>
2020-01-31 12:46:35 -0800

    Some more refactoring

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -1,40 +1,36 @@
-import React, { Component } from 'react';
+import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 
-class LoadingSpinner extends Component {
-  render() {
-    const styles = {
-      root: {
-        flexGrow: 1,
-        width: '80%',
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        marginTop: 60
-      },
-      paper: {
-        padding: 20,
-        display: 'flex',
-        width: '100%',
-        justifyContent: 'center'
-      }
-    };
-
-    return (
-      <div style={styles.root}>
-        <Grid container spacing={24}>
-          <Grid item sm={12}>
-            <Paper style={styles.paper}>
-              <Paper style={styles.paper}>
-                <CircularProgress color="secondary" />
-              </Paper>
-            </Paper>
-          </Grid>
-        </Grid>
-      </div>
-    );
+const styles = {
+  root: {
+    flexGrow: 1,
+    width: '80%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    marginTop: 60
+  },
+  paper: {
+    padding: 20,
+    display: 'flex',
+    width: '100%',
+    justifyContent: 'center'
   }
-}
+};
+
+const LoadingSpinner = () => (
+  <div style={styles.root}>
+    <Grid container spacing={24}>
+      <Grid item sm={12}>
+        <Paper style={styles.paper}>
+          <Paper style={styles.paper}>
+            <CircularProgress color="secondary" />
+          </Paper>
+        </Paper>
+      </Grid>
+    </Grid>
+  </div>
+);
 
 export default LoadingSpinner;
