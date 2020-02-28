b29ffa7702311c2b2776b3f21a51b14ec156df07
Kyle Bacon <hello@kylebaconcreative.com>
2020-02-28 13:56:39 -0800

    Email editor only refactor

--- src/Components/Email/EmailEditorOnly.js
+++ src/Components/Email/EmailEditorOnly.js
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import gql from 'graphql-tag';
-import { withApollo, graphql } from 'react-apollo';
-import flowright from 'lodash.flowright';
-import { withRouter } from 'react-router-dom';
+import { useQuery, useMutation } from 'react-apollo';
+import { useParams } from 'react-router-dom';
 
 import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
@@ -16,180 +15,23 @@ import 'codemirror/mode/javascript/javascript';
 
 import Loading from '../../Components/Loading/Loading';
 
-class EmailEditorOnly extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      title: '',
-      mjmlSource: '',
-      folderPath: '',
-      errorMessage: ''
-    };
-    this.editEmailPartial = this.editEmailPartial.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.goBack = this.goBack.bind(this);
+const styles = {
+  root: {
+    flexGrow: 1
+  },
+  container: {
+    padding: 20
+  },
+  paper: {
+    padding: 20
+  },
+  iframe: {
+    width: '100%',
+    height: '80vh'
   }
+};
 
-  editEmailPartial() {
-    const { title, mjmlSource, folderPath } = this.state;
-    const organizationId =
-      !this.props.currentUser.loading &&
-      this.props.currentUser.currentUser &&
-      this.props.currentUser.currentUser.organizationId;
-    const _id = this.props.match.params.id;
-
-    this.props
-      .editEmailPartial({
-        variables: {
-          _id,
-          title,
-          mjmlSource,
-          folderPath,
-          organizationId
-        },
-        refetchQueries: [`getCurrentEmailPartial`]
-      })
-      .catch(error => {
-        console.error(error);
-        this.setState({
-          errorMessage: error.message.split(':')[1]
-        });
-      });
-  }
-
-  handleChange(e) {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  }
-
-  goBack() {
-    this.props.history.goBack();
-  }
-
-  componentWillReceiveProps(newProps) {
-    if (!newProps.getCurrentEmailPartial.loading) {
-      const { title, mjmlSource, folderPath } =
-        !newProps.getCurrentEmailPartial.loading &&
-        newProps.getCurrentEmailPartial.getCurrentEmailPartial;
-      this.setState({
-        title,
-        mjmlSource,
-        folderPath
-      });
-    }
-  }
-
-  render() {
-    const styles = {
-      root: {
-        flexGrow: 1
-      },
-      container: {
-        padding: 20
-      },
-      paper: {
-        padding: 20
-      },
-      iframe: {
-        width: '100%',
-        height: '80vh'
-      }
-    };
-
-    if (this.props.getCurrentEmailPartial.loading) return <Loading />;
-
-    return (
-      <div style={styles.root}>
-        <div style={styles.container}>
-          <Grid container spacing={24}>
-            <Grid item sm={12}>
-              <Paper style={styles.paper}>
-                <Grid container spacing={24}>
-                  <Grid item sm={5}>
-                    <Typography variant="h6" component="h4">
-                      Email Partial Editor
-                    </Typography>
-                    <TextField
-                      name="title"
-                      placeholder="title"
-                      fullWidth
-                      onChange={this.handleChange}
-                      value={this.state.title || ''}
-                    />
-                  </Grid>
-                  <Grid item sm={4}>
-                    <TextField
-                      name="folderPath"
-                      placeholder="folderPath"
-                      fullWidth
-                      onChange={this.handleChange}
-                      value={this.state.folderPath || ''}
-                      style={{ marginTop: 23 }}
-                    />
-                  </Grid>
-                  <Grid
-                    item
-                    sm={3}
-                    style={{
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'flex-end'
-                    }}
-                  >
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      size="small"
-                      onClick={this.editEmailPartial}
-                      style={{ marginLeft: 10 }}
-                    >
-                      Save
-                    </Button>
-                    <Button
-                      variant="outlined"
-                      size="small"
-                      style={{ marginLeft: 10 }}
-                    >
-                      Duplicate
-                    </Button>
-                    <Button variant="outlined" size="small" color="secondary">
-                      Delete
-                    </Button>
-                  </Grid>
-                </Grid>
-              </Paper>
-            </Grid>
-          </Grid>
-        </div>
-        <Grid container spacing={24}>
-          <Grid item xs={12}>
-            <CodeMirror
-              value={this.state.mjmlSource}
-              options={{
-                mode: 'xml',
-                theme: 'material',
-                lineNumbers: true,
-                lineWrapping: true
-              }}
-              onBeforeChange={(editor, data, value) => {
-                this.setState({
-                  mjmlSource: value
-                });
-              }}
-              onChange={(editor, data, value) => {
-                this.setState({
-                  mjmlSource: value
-                });
-              }}
-            />
-          </Grid>
-        </Grid>
-      </div>
-    );
-  }
-}
-
-const currentUser = gql`
+const CURRENT_USER = gql`
   query currentUser {
     currentUser {
       _id
@@ -198,7 +40,7 @@ const currentUser = gql`
   }
 `;
 
-const getCurrentEmailPartial = gql`
+const GET_CURRENT_EMAIL_PARTIAL = gql`
   query getCurrentEmailPartial($_id: String!) {
     getCurrentEmailPartial(_id: $_id) {
       _id
@@ -209,7 +51,7 @@ const getCurrentEmailPartial = gql`
   }
 `;
 
-const editEmailPartial = gql`
+const EDIT_EMAIL_PARTIAL = gql`
   mutation editEmailPartial(
     $_id: String!
     $title: String!
@@ -229,20 +71,157 @@ const editEmailPartial = gql`
   }
 `;
 
-export default withRouter(
-  flowright(
-    graphql(currentUser, {
-      name: 'currentUser'
-    }),
-    graphql(getCurrentEmailPartial, {
-      name: 'getCurrentEmailPartial',
-      options: ownProps => ({
-        variables: { _id: ownProps.match.params.id },
-        forceRefetch: true
-      })
-    }),
-    graphql(editEmailPartial, {
-      name: 'editEmailPartial'
-    })
-  )(withApollo(EmailEditorOnly))
-);
+const EmailEditorOnly = () => {
+  const { id } = useParams();
+  const [value, setValue] = useState({
+    title: '',
+    mjmlSource: '',
+    folderPath: '',
+    errorMessage: ''
+  });
+  const { data: userData, loading: userLoading } = useQuery(CURRENT_USER);
+  const {
+    data: getCurrentEmailPartialData,
+    loading: getCurrentEmailPartialLoading
+  } = useQuery(GET_CURRENT_EMAIL_PARTIAL, {
+    variables: {
+      _id: id
+    }
+  });
+  const [
+    editEmailPartial,
+    { loading: editEmailPartialLoading, error: editEmailPartialError }
+  ] = useMutation(EDIT_EMAIL_PARTIAL);
+
+  console.log(editEmailPartialError);
+
+  const handleEditEmailPartial = () => {
+    const { title, mjmlSource, folderPath } = value;
+    const organizationId =
+      !userLoading &&
+      userData.currentUser &&
+      userData.currentUser.organizationId;
+
+    editEmailPartial({
+      variables: {
+        _id: id,
+        title,
+        mjmlSource,
+        folderPath,
+        organizationId
+      }
+    });
+  };
+
+  const handleChange = e =>
+    setValue({
+      ...value,
+      [e.target.name]: e.target.value
+    });
+
+  useEffect(() => {
+    if (getCurrentEmailPartialLoading) return;
+
+    setValue(v => ({
+      ...v,
+      ...getCurrentEmailPartialData.getCurrentEmailPartial
+    }));
+  }, [getCurrentEmailPartialData, getCurrentEmailPartialLoading]);
+
+  if (getCurrentEmailPartialLoading) return <Loading />;
+
+  return (
+    <div style={styles.root}>
+      <div style={styles.container}>
+        <Grid container spacing={24}>
+          <Grid item sm={12}>
+            <Paper style={styles.paper}>
+              <Grid container spacing={24}>
+                <Grid item sm={5}>
+                  <Typography variant="h6" component="h4">
+                    Email Partial Editor
+                  </Typography>
+                  <TextField
+                    name="title"
+                    placeholder="title"
+                    fullWidth
+                    onChange={handleChange}
+                    value={value.title || ''}
+                  />
+                </Grid>
+                <Grid item sm={4}>
+                  <TextField
+                    name="folderPath"
+                    placeholder="folderPath"
+                    fullWidth
+                    onChange={handleChange}
+                    value={value.folderPath || ''}
+                    style={{ marginTop: 23 }}
+                  />
+                </Grid>
+                <Grid
+                  item
+                  sm={3}
+                  style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'flex-end'
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    size="small"
+                    onClick={handleEditEmailPartial}
+                    style={{ marginLeft: 10 }}
+                  >
+                    Save
+                  </Button>
+                  {/* <Button
+                    variant="outlined"
+                    size="small"
+                    style={{ marginLeft: 10 }}
+                  >
+                    Duplicate
+                  </Button>
+                  <Button variant="outlined" size="small" color="secondary">
+                    Delete
+                  </Button> */}
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </div>
+      <Grid container spacing={24}>
+        <Grid item xs={12}>
+          {editEmailPartialLoading && <p>Loading...</p>}
+          {editEmailPartialError && <p>Error :( Please try again</p>}
+          <CodeMirror
+            value={value.mjmlSource}
+            options={{
+              mode: 'xml',
+              theme: 'material',
+              lineNumbers: true,
+              lineWrapping: true
+            }}
+            onBeforeChange={(editor, data, value) => {
+              setValue(v => ({
+                ...v,
+                mjmlSource: value
+              }));
+            }}
+            onChange={(editor, data, value) => {
+              setValue(v => ({
+                ...v,
+                mjmlSource: value
+              }));
+            }}
+          />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default EmailEditorOnly;
