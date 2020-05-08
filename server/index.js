800d7d029409f2e1e6df4bf871ef0795cb0087c3
Kyle Bacon <hello@kylebaconcreative.com>
2020-05-08 10:17:09 -0700

    Upgrade node, downgrade mjml

--- server/index.js
+++ server/index.js
@@ -12,9 +12,13 @@ const gqlPath = '/';
 
 if (!process.env.MONGO_URI) throw new Error('You must provide a MongoLab URI');
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, err => {
-  if (err) console.error('Could not connect to MongoDB');
-});
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (err) => {
+    if (err) console.error('Could not connect to MongoDB');
+  }
+);
 
 const server = new ApolloServer({
   typeDefs,
